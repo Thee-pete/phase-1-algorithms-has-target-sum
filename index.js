af5b816afb4b1665f9d7536bd839a718e93b540c
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-19 04:22:35 +0300

    commit tree 00b6d8a9447d826d5f86c457faaf898011f678f4

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i =0 ; i < array.length; i++){
+    for (let j= i+1; j < array.length; j++){
+
+      if ((array[i] + array[j]) === target){
+        return true
+      }
+    }
+  }
+  return false
+  
 }
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+iterate throught array chccking if the first number added to other numbers equal target an return true id equal
+iterate a second time checking if the second number added to other numbers equal targeet and return true
+if either iteration doesnt return true then return false after all iterations
+Add your pseudocode here
 */
 
 /*
